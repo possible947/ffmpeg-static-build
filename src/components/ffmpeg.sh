9f1ebfdbc5b892c6f3e3235472d25a47082f83bf
possible947 <possible947@gmail.com>
2026-03-26 14:52:43 +0200

    refactor: split ffmpeg final stage into atomic functions

--- src/components/ffmpeg.sh
+++ src/components/ffmpeg.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
 
-build_ffmpeg_and_install() {
-  ## FFmpeg
-  ##
-  
-  EXTRA_VERSION=""
+ffmpeg_require_option() {
+  local required="$1"
+  local found=false
+
+  for opt in "${CONFIGURE_OPTIONS[@]}"; do
+    if [ "$opt" = "$required" ]; then
+      found=true
+      break
+    fi
+  done
+
+  if ! $found; then
+    echo "Error: required FFmpeg configure option is missing: $required"
+    exit 1
+  fi
+}
+
+verify_required_ffmpeg_options() {
+  ffmpeg_require_option "--enable-gpl"
+  ffmpeg_require_option "--enable-nonfree"
+  ffmpeg_require_option "--enable-libfdk-aac"
+  ffmpeg_require_option "--enable-libvmaf"
+  ffmpeg_require_option "--enable-libsoxr"
+  ffmpeg_require_option "--enable-vulkan"
+  ffmpeg_require_option "--enable-opencl"
+
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    EXTRA_VERSION="${FFMPEG_VERSION}"
+    ffmpeg_require_option "--enable-videotoolbox"
   fi
-  
+}
+
+prepare_ffmpeg_source() {
   if [ -d "$CWD/.git" ]; then
-    echo -e "\nTemporarily moving .git dir to .git.bak to workaround ffmpeg build bug" #causing ffmpeg version number to be wrong
+    echo -e "\nTemporarily moving .git dir to .git.bak to workaround ffmpeg build bug"
     mv "$CWD/.git" "$CWD/.git.bak"
-    # if build fails below, .git will remain in the wrong place...
   fi
-  
+
   build "ffmpeg" "$FFMPEG_VERSION"
   download "https://github.com/FFmpeg/FFmpeg/archive/refs/tags/n$FFMPEG_VERSION.tar.gz" "FFmpeg-release-$FFMPEG_VERSION.tar.gz"
+}
+
+configure_ffmpeg() {
+  local extra_version=""
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    extra_version="${FFMPEG_VERSION}"
+  fi
+
   # shellcheck disable=SC2086
-  
   if $FULL_STATIC; then
     execute ./configure "${CONFIGURE_OPTIONS[@]}" \
       --disable-debug \
@@ -33,7 +63,7 @@ build_ffmpeg_and_install() {
       --pkgconfigdir="$WORKSPACE/lib/pkgconfig" \
       --pkg-config-flags="--static" \
       --prefix="${WORKSPACE}" \
-      --extra-version="${EXTRA_VERSION}"
+      --extra-version="${extra_version}"
   else
     execute ./configure "${CONFIGURE_OPTIONS[@]}" \
       --disable-debug \
@@ -46,67 +76,78 @@ build_ffmpeg_and_install() {
       --extra-libs="${EXTRALIBS}" \
       --pkgconfigdir="$WORKSPACE/lib/pkgconfig" \
       --prefix="${WORKSPACE}" \
-      --extra-version="${EXTRA_VERSION}"
+      --extra-version="${extra_version}"
   fi
-  
-  execute make -j "$MJOBS"
-  execute make install
-  
-  if [ -d "$CWD/.git.bak" ]; then
-    mv "$CWD/.git.bak" "$CWD/.git"
-  fi
-  
-  INSTALL_FOLDER="/usr"  # not recommended, overwrites system ffmpeg package
+}
+
+install_ffmpeg_to_system() {
+  local install_folder="/usr"
+  local install_now=0
+
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    INSTALL_FOLDER="/usr/local"
+    install_folder="/usr/local"
   else
-    if [ -d "$HOME/.local" ]; then  # systemd-standard user path
-      INSTALL_FOLDER="$HOME/.local"
+    if [ -d "$HOME/.local" ]; then
+      install_folder="$HOME/.local"
     elif [ -d "/usr/local" ]; then
-      INSTALL_FOLDER="/usr/local"
+      install_folder="/usr/local"
     fi
   fi
-  
-  verify_binary_type
-  
-  echo ""
-  echo "Building done. The following binaries can be found here:"
-  echo "- ffmpeg: $WORKSPACE/bin/ffmpeg"
-  echo "- ffprobe: $WORKSPACE/bin/ffprobe"
-  echo "- ffplay: $WORKSPACE/bin/ffplay"
-  echo ""
-  
-  INSTALL_NOW=0
+
   if [[ "$AUTOINSTALL" == "yes" ]]; then
-    INSTALL_NOW=1
+    install_now=1
     echo "Automatically installing these binaries because the --auto-install option was used or AUTOINSTALL=yes was run."
   elif [[ ! "$SKIPINSTALL" == "yes" ]]; then
-    read -r -p "Install these binaries to your $INSTALL_FOLDER folder? Existing binaries will be replaced. [Y/n] " response
+    read -r -p "Install these binaries to your $install_folder folder? Existing binaries will be replaced. [Y/n] " response
     case $response in
       "" | [yY][eE][sS] | [yY])
-        INSTALL_NOW=1
+        install_now=1
       ;;
     esac
   else
     echo "Skipping install of these binaries because the --skip-install option was used or SKIPINSTALL=yes was run."
   fi
-  
-  if [ "$INSTALL_NOW" = 1 ]; then
-    if command_exists "sudo" && [[ $INSTALL_FOLDER == /usr* ]]; then
+
+  if [ "$install_now" = 1 ]; then
+    if command_exists "sudo" && [[ $install_folder == /usr* ]]; then
       SUDO=sudo
     fi
-    $SUDO cp "$WORKSPACE/bin/ffmpeg" "$INSTALL_FOLDER/bin/ffmpeg"
-    $SUDO cp "$WORKSPACE/bin/ffprobe" "$INSTALL_FOLDER/bin/ffprobe"
-    $SUDO cp "$WORKSPACE/bin/ffplay" "$INSTALL_FOLDER/bin/ffplay"
+    $SUDO cp "$WORKSPACE/bin/ffmpeg" "$install_folder/bin/ffmpeg"
+    $SUDO cp "$WORKSPACE/bin/ffprobe" "$install_folder/bin/ffprobe"
+    $SUDO cp "$WORKSPACE/bin/ffplay" "$install_folder/bin/ffplay"
     if [ $MANPAGES = 1 ]; then
-      $SUDO mkdir -p "$INSTALL_FOLDER/share/man/man1"
-      $SUDO cp "$WORKSPACE/share/man/man1"/ff* "$INSTALL_FOLDER/share/man/man1"
+      $SUDO mkdir -p "$install_folder/share/man/man1"
+      $SUDO cp "$WORKSPACE/share/man/man1"/ff* "$install_folder/share/man/man1"
       if command_exists "mandb"; then
         $SUDO mandb -q
       fi
     fi
     echo "Done. FFmpeg is now installed to your system."
   fi
-  
+}
+
+build_ffmpeg_and_install() {
+  verify_required_ffmpeg_options
+  prepare_ffmpeg_source
+  configure_ffmpeg
+
+  execute make -j "$MJOBS"
+  execute make install
+
+  if [ -d "$CWD/.git.bak" ]; then
+    mv "$CWD/.git.bak" "$CWD/.git"
+  fi
+
+  verify_binary_type
+
+  echo ""
+  echo "Building done. The following binaries can be found here:"
+  echo "- ffmpeg: $WORKSPACE/bin/ffmpeg"
+  echo "- ffprobe: $WORKSPACE/bin/ffprobe"
+  echo "- ffplay: $WORKSPACE/bin/ffplay"
+  echo ""
+
+  install_ffmpeg_to_system
+
   return 0
 }
